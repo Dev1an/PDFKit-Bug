c9b330b000485733eb24fdeebe342f9af5f2e91c
Damiaan Dufaux <damiaan@dpro.be>
2017-11-29 13:15:08 +0100

    Initial Commit

---
+++ pdfKitBug/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pdfKitBug
+//
+//  Created by Damiaan on 29/11/17.
+//  Copyright © 2017 Devian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
